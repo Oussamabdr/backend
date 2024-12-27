3eac6e4b5424f4b94d6fe378f7550667df0a3e76
oussama bedraoui <mo_bedraoui@esi.dz>
2024-12-28 00:54:52 +0100

    admin

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views  # Import views from your application
 
-app_name = 'backApp'
+app_name = 'app'
 urlpatterns = [
     path('patients/', views.patient_list, name='patient_list'),  # List patients
     path('patients/<int:id>/', views.patient_detail, name='patient_detail'), # Patient details by ID
